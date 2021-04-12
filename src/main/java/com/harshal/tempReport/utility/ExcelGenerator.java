f01da28d89f0f12d19b2857ecc07e140ce3d34f0
Kunjan Patel <kunjan.dk@gmail.com>
2021-04-12 10:28:35 -0400

    writing excel to client machine

--- src/main/java/com/harshal/tempReport/utility/ExcelGenerator.java
+++ src/main/java/com/harshal/tempReport/utility/ExcelGenerator.java
@@ -1,13 +1,43 @@
 package com.harshal.tempReport.utility;
 
 import com.harshal.tempReport.model.ToDo;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 public class ExcelGenerator {
-    public static ByteArrayInputStream dataToExcel(List<ToDo> toDoList) {
+    public static ByteArrayInputStream dataToExcel(List<ToDo> toDoList) throws IOException {
 
-        
+        String columns[] = {"ToDo Name", "Description"};
+        try(Workbook workbook = new XSSFWorkbook(); ByteArrayOutputStream out = new ByteArrayOutputStream();) {
+            CreationHelper creationHelper = workbook.getCreationHelper();
+            Sheet sheet = workbook.createSheet("Report1");
+
+            Font headerFont = workbook.createFont();
+            headerFont.setBold(true);
+            headerFont.setColor(IndexedColors.BLUE.getIndex());
+
+            CellStyle headerCellStyle = workbook.createCellStyle();
+            headerCellStyle.setFont(headerFont);
+
+            //Row for header
+            Row headerRow = sheet.createRow(0);
+
+            //Header
+            for(int col = 0; col<columns.length; col++) {
+                Cell cell = headerRow.createCell(col);
+                cell.setCellValue(columns[col]);
+                cell.setCellStyle(headerCellStyle);
+            }
+
+            int rowInx = 1;
+            for (ToDo toDo : toDoList) {
+
+            }
+        }
     }
 }
